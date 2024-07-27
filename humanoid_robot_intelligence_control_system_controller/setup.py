34e58ce0eb39637060b3544e88b16d94b7d128e0
RonaldsonBellande <ronaldsonbellande@gmail.com>
2024-07-27 08:13:07 -0400

    latest pushes

--- humanoid_robot_intelligence_control_system_controller/setup.py
+++ humanoid_robot_intelligence_control_system_controller/setup.py
@@ -18,7 +18,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    scripts=['src/PIDController.py'],
+    scripts=['src/PIDController.py', "src/BellandeController.py"],
     packages=['humanoid_robot_intelligence_control_system_controller'],
     package_dir={'': 'src'},
 )
